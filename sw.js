bac5ee61f3825c3cc56762151492ba03fa8d87b9
Raffaele Morí <mori.raffaele@gmail.com>
2020-12-28 17:19:45 +0000

    Fixed address for index.html and index.js.

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 const CACHE_NAME= 'v1';
 
 const FILE_LIST = [
-    'index.html',
+    './index.html',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/webfonts/fa-solid-900.woff2',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/webfonts/fa-solid-900.woff',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/webfonts/fa-solid-900.ttf',
     './style.css',
-    'index.js'
+    './index.js'
 ];
 
 self.addEventListener('install', (e) => {
